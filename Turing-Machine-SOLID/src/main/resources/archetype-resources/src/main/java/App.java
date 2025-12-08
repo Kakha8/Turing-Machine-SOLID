79747f632e579490ffa1b9088ca64c2abe43dcdd
Kakha8 <kakha.kudava.98@gmail.com>
2025-12-08 16:59:53 +0200

    initial upload

---
+++ Turing-Machine-SOLID/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $kakha.kudava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
